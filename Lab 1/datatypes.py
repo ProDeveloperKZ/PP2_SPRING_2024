1103b36c8ae64e3a12c376fd3d273e6fa20d7b44
Tampekov Rassul <156893000+ProDeveloperKZ@users.noreply.github.com>
2024-01-24 22:24:02 +0600

    Add files via upload

---
+++ Lab 1/datatypes.py
@@ -0,0 +1,48 @@
+#task 1
+
+print(type(x))
+
+#output: int
+
+#task 2
+
+x = "Hello World"
+print(type(x))
+
+#Output: str
+
+#task 3
+
+x = 20.5
+print(type(x))
+
+#output: float
+
+#task 4
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#output: list
+
+#task 5
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#output: tuple
+
+#task 6
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#output: dict
+
+#task 7
+
+x = True
+print(type(x))
+
+#output: bool
+
---
+++ Lab 1/syntax.py
@@ -0,0 +1,10 @@
+#task 1
+
+print("Hello World")
+
+#task 2
+
+if 5 > 2:
+    print("YES")
+
+
