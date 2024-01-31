580d9eed1a9a4215e10ff908a8480dd85c2fb8a4
Tampekov Rassul <156893000+ProDeveloperKZ@users.noreply.github.com>
2024-01-31 17:36:35 +0600

    Add files via upload

---
+++ Lab 2/booleans.py
@@ -0,0 +1,25 @@
+#task 1 
+
+print(10 > 9)
+#output: True
+
+#task 2
+
+print(10 == 9)
+#output: False
+
+#task 3
+
+print(10 < 9)
+#output: False
+
+#task 4
+
+print(bool("abc"))
+#output: True
+
+#task 5
+
+print(bool(0))
+#output: False
+
